65e4ab60ba543fb6ea6406cac2c5ffbe1508c747
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-09-08 20:16:01 -0400

    Initial commit

---
+++ webscraping/charts_scraping.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+'''
+iChart:
+    Realtime: https://www.instiz.net/iframe_ichart_score.htm?real=1
+
+MelonChart:
+    Realtime: http://www.melon.com/chart/index.htm
+
+Gaon Chart:
+    Daily: https://circlechart.kr/page_chart/global.circle?termGbn=day
+
+Billboard K-Pop 100:
+    https://www.billboard.com/charts/billboard-korea-100/
+'''
